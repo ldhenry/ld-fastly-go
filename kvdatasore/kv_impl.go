5901e496fa248670d5687cf48f6121b62f6c0191
Henry Barrow <hbarrow@launchdarkly.com>
2025-03-03 16:42:37 +0000

    Implement GetAll

--- kvdatasore/kv_impl.go
+++ kvdatasore/kv_impl.go
@@ -67,25 +67,35 @@ type allDataStruct struct {
 	Flags map[string]interface{} `json:"flags"`
 }
 
-func (store *fastlyKVDataStoreImpl) Get(kind ldstoretypes.DataKind, key string) (ldstoretypes.SerializedItemDescriptor, error) {
-	store.loggers.Info("Getting item from FastlyKVDataStore")
+func (store *fastlyKVDataStoreImpl) getAllFlagData() (allDataStruct, error) {
 	o, err := store.getKV()
 	if err != nil {
-		return ldstoretypes.SerializedItemDescriptor{}, err
+		return allDataStruct{}, err
 	}
 
 	v, err := o.Lookup(ENV_KEY_PREFIX + store.clientSideID)
 	if err != nil {
-		return ldstoretypes.SerializedItemDescriptor{}, err
+		return allDataStruct{}, err
 	}
 
 	var allData allDataStruct
 	err = json.Unmarshal([]byte(v.String()), &allData)
 	if err != nil {
+		return allDataStruct{}, err
+	}
+
+	return allData, nil
+}
+
+func (store *fastlyKVDataStoreImpl) Get(kind ldstoretypes.DataKind, key string) (ldstoretypes.SerializedItemDescriptor, error) {
+	store.loggers.Info("Getting item from FastlyKVDataStore")
+
+	allData, err := store.getAllFlagData()
+	if err != nil {
 		return ldstoretypes.SerializedItemDescriptor{}, err
 	}
 
-	flag, ok := allData.Flags["animal"]
+	flag, ok := allData.Flags[key]
 	if !ok {
 		return ldstoretypes.SerializedItemDescriptor{}, fmt.Errorf("flag not found")
 	}
@@ -102,9 +112,29 @@ func (store *fastlyKVDataStoreImpl) Get(kind ldstoretypes.DataKind, key string) 
 
 func (store *fastlyKVDataStoreImpl) GetAll(kind ldstoretypes.DataKind) ([]ldstoretypes.KeyedSerializedItemDescriptor, error) {
 	store.loggers.Info("Getting all items from FastlyKVDataStore")
-	return nil, nil
+
+	allData, err := store.getAllFlagData()
+	if err != nil {
+		return nil, err
+	}
+
+	flagDescriptors := make([]ldstoretypes.KeyedSerializedItemDescriptor, 0, len(allData.Flags))
+	for k, v := range allData.Flags {
+		flagBytes, err := json.Marshal(v)
+		if err != nil {
+			return nil, err
+		}
+		flagDescriptors = append(flagDescriptors, ldstoretypes.KeyedSerializedItemDescriptor{
+			Key: k,
+			Item: ldstoretypes.SerializedItemDescriptor{
+				SerializedItem: flagBytes,
+			},
+		})
+	}
+	return flagDescriptors, nil
 }
 
+// Upsert should not be needed since the KV store is updated by LaunchDarkly directly
 func (store *fastlyKVDataStoreImpl) Upsert(
 	kind ldstoretypes.DataKind,
 	key string,
